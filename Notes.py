3add11e71f3df9111c13902a493672605267427f
KELLY OSADIAYE <52336604+Kosadiaye@users.noreply.github.com>
2019-12-03 15:17:03 -0500

    added python notes

---
+++ Notes.py
@@ -0,0 +1,13 @@
+
+# print("hello world")
+
+def adder(a,b):
+    return a-b
+
+# print(adder(5,9))
+mystring = "HelloWorld"
+x = 5
+#print(x, mystring[:5], mystring[5:])
+mynumbers ="123456789"
+print(mynumbers[::-3])
+
